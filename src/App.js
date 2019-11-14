2d8f198a186ef33132415f0dd669d4ea24e9d28e
Alex Yaro <alxyaro@gmail.com>
2019-11-13 19:54:27 -0500

    Package updates, added bootstrap, base template.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			/*number : 0*/
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="container">
+					<div className="row">
+						<div className="col-3 actions">
+							<h3>Actions</h3>
+							<button className="btn btn-primary">Create Tables</button>
+							<button className="btn btn-primary" disabled>Add Data</button>
+							<button className="btn btn-primary" disabled>Run Sample Queries</button>
+							<button className="btn btn-primary" disabled>Drop Tables</button>
+							<br/><br/>
+							<button className="btn btn-warning" disabled>Lookup Movies</button>
+						</div>
+						<div className="col-9">
+							<h3>Main Content</h3>
+							<p>Cras elementum dolor nec massa egestas, vitae fermentum ipsum sollicitudin. Nulla nisi est, porttitor vel velit eu, pellentesque luctus nibh. In blandit id magna id ornare. Praesent imperdiet diam sed est condimentum imperdiet. Nunc tincidunt ipsum felis, volutpat ornare nulla maximus sit amet. In hac habitasse platea dictumst. Sed in libero ipsum. Pellentesque rutrum in tortor id blandit. Etiam risus ipsum, pretium in magna nec, accumsan gravida dui. Vestibulum a dolor tempor, ullamcorper dolor ac, lacinia sapien. Pellentesque sit amet ultricies mauris. Praesent luctus dui nisi, quis viverra turpis auctor eu.</p>
+							<pre>
+								<code>
+									SQL output goes here<br/>and here
+								</code>
+							</pre>
+						</div>
+					</div>
+				</div>
+				{/*<p className="App-intro">
+					The current value of number is {this.state.number}
+				</p>
+				<button onClick={()=>this.setState({number : this.state.number + 1})}>+</button>*/}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './bootstrap.min.css';
 import './index.css';
 
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+	rootEl
 );
+
+if (module.hot) {
+	console.log('HOT');
+	module.hot.accept('./App', () => {
+		const NextApp = require('./App').default
+		ReactDOM.render(
+			<NextApp />,
+			rootEl
+		)
+	})
+}
